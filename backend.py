068bd1efc6bb7750fa05c5bc95a19cd03440a079
Rali Venkata Ram Rishik <157054266+RishikRVR@users.noreply.github.com>
2025-04-23 13:11:07 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -168,8 +168,7 @@ def chat():
 
 # --- Init Databases ---
 with app.app_context():
-    db.create_all(bind='users')
-    db.create_all(bind='admins')
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
